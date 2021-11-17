f6e339e22f885f0a3a39015a3e7dd6f1b62a56de
atharsajid <athersajid620@gmail.com>
2021-11-17 08:49:18 -0800

    update

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:hackathone/Screens/login_screen/loginscreen.dart';
+import 'package:hackathone/Screens/splashscreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashScreen(),
+    );
+  }
+}
